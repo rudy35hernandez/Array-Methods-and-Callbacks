a22e9fd5f5bd0f0ae0e8c8fe09ba06842127049a
rudy35hernandez <rudy35hernandez@gmail.com>
2020-10-03 23:10:27 -0700

    Finished Task 3. Keep going!

--- index.js
+++ index.js
@@ -44,11 +44,17 @@ function getFinals(data) {
 
 /* Task 3: Implement a higher-order function called `getYears` that accepts the callback function `getFinals`, and returns an array called `years` containing all of the years in the dataset */
 
-function getYears(/* code here */) {
-
-    /* code here */
-
-};
+function getYears(cbGetFinals) {
+    const finalsYes = 
+    cbGetFinals("Final").map((item) => {
+      return {
+        'Year': item.Year
+      }
+    });
+        return finalsYes;
+        }
+ 
+        console.log(getYears(getFinals))
 
 getYears();
 
